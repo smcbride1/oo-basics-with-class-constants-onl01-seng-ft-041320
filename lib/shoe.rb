aabb79b0bafa852fad7112d88de1a21002d4aeba
Stephen McBride <stevemcbride3@gmail.com>
2020-04-22 05:23:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.push(brand) if BRANDS.detect { |b| b == brand }
+    BRANDS.push(brand) if BRANDS.include?(brand)
   end
 
   def cobble
